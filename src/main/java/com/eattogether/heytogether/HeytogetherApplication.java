7f8e3fbc743b8b03d04f43ee02b993dabe491657
starkim06 <starkim06@naver.com>
2019-12-02 14:56:13 +0900

    프로젝트 생성

---
+++ src/main/java/com/eattogether/heytogether/HeytogetherApplication.java
@@ -0,0 +1,13 @@
+package com.eattogether.heytogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeytogetherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeytogetherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eattogether/heytogether/HeytogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eattogether.heytogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeytogetherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
