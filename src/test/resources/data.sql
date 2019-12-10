9f60b0434d5c0fe6620d687d89d348de79cea006
yeji <yjll1019@naver.com>
2019-12-10 17:49:54 +0900

    chore: open-in-view false 설정 해제, data.sql 데이터 추가

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,3 +1,5 @@
 insert into place(latitude, longitude) values (3.3, 5.5);
 
 insert into shop(delivery_tip, minimum_order_price, place_id) values (5000, 18000, 1);
+
+insert into shop(delivery_tip, minimum_order_price, place_id) values (3000, 15000, 1);
