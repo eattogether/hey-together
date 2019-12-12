58a189dc6ae049cb25ea9c7da133eddadb1e917a
YejiLee <32984517+yjll1019@users.noreply.github.com>
2019-12-12 17:30:25 +0900

    [#11] feat: 가게, 메뉴 기능 구현 (#12)
    * chore: DB 관련 설정 추가
    
    * feat: 가게 등록 기능 구현
    
    * chore: test DB 설정 및 데이터 추가
    
    * feat: 가게 조회 기능 구현
    
    * chore: open-in-view false 설정 해제, data.sql 데이터 추가
    
    * chore: webflux, mysql-connector 의존성 추가
    
    * feat: 가게의 메뉴 추가 기능 구현
    
    * refactor: controller 패키지 수정
    
    * refactor: Money 객체 생성, 도메인 객체의 setter 삭제
    
    * refactor: equals(), hashCode(), toString() 재정의
    
    * refactor: MenuAssembler, ShopAssembler 생성
    
    * [#11] refactor: 개행 컨벤션
    
    * [#11] refactor: 불필요한 테스트 클래스 삭제
    
    * [#11] refactor: 피드백 반영
    
    - gradle impl -> testImpl
    - GenerationType.Identity -> AUTO
    - CascadeType.Merge -> ALL
    - ShopApiControllerTest -> ShopApiAcceptanceTest
    - @DisplayName 설명 추가
    
    * [#11] refactor: Place를 Entity에서 VO로 변경
    
    * [#11] chore: hibernate id 생성 옵션 설정 추가
    
    * [#11] refactor: MenuInfoDto, ShopInfoDto 생성
    
    * [#11] chore: junit-jupiter-api 의존성 추가
    
    * [#11] test: MenuServiceTest, ShopServiceTest 추가
    
    * [#11] refactor: @JsonBackReference, @JsonManagedReference 어노테이션 삭제

---
+++ src/main/java/com/eattogether/heytogether/domain/Place.java
@@ -0,0 +1,54 @@
+package com.eattogether.heytogether.domain;
+
+import java.util.Objects;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Place {
+
+    private double longitude;
+
+    private double latitude;
+
+    public Place() {
+    }
+
+    public Place(double longitude, double latitude) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Place)) {
+            return false;
+        }
+        final Place place = (Place) o;
+        return Double.compare(place.getLongitude(), getLongitude()) == 0 &&
+                Double.compare(place.getLatitude(), getLatitude()) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getLongitude(), getLatitude());
+    }
+
+    @Override
+    public String toString() {
+        return "Place{" +
+                "longitude=" + longitude +
+                ", latitude=" + latitude +
+                '}';
+    }
+}
---
+++ src/test/java/com/eattogether/heytogether/service/MenuServiceTest.java
@@ -0,0 +1,50 @@
+package com.eattogether.heytogether.service;
+
+import com.eattogether.heytogether.domain.Money;
+import com.eattogether.heytogether.domain.repository.MenuRepository;
+import com.eattogether.heytogether.service.dto.MenuCreateDto;
+import com.eattogether.heytogether.service.exception.ShopNotFoundException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(SpringExtension.class)
+class MenuServiceTest {
+
+    @InjectMocks
+    private MenuService menuService;
+
+    @Mock
+    private MenuRepository menuRepository;
+
+    @Mock
+    private ShopService shopService;
+
+    @Test
+    @DisplayName("메뉴 추가 성공")
+    void save_menu() {
+        MenuCreateDto menuCreateDto = new MenuCreateDto("pizza", new Money(23000));
+
+        menuService.save(1L, menuCreateDto);
+        verify(menuRepository, Mockito.times(1)).save(any());
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 가게의 id에 메뉴 추가 시 예외 발생")
+    void save_menu_with_shopNotFoundException() {
+        MenuCreateDto menuCreateDto = new MenuCreateDto("pizza", new Money(23000));
+        given(shopService.findEntityBy(-1L)).willThrow(ShopNotFoundException.class);
+
+        assertThrows(ShopNotFoundException.class, () -> menuService.save(-1L, menuCreateDto));
+    }
+}
