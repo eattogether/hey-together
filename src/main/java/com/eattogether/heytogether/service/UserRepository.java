5ace7445fa917257cd4621920906a72ad5ab1bf8
starkim06 <starkim06@naver.com>
2019-12-13 17:02:03 +0900

    [#16] style: 레포지토리 어노테이션 추가

--- src/main/java/com/eattogether/heytogether/service/UserRepository.java
+++ src/main/java/com/eattogether/heytogether/service/UserRepository.java
@@ -2,9 +2,12 @@ package com.eattogether.heytogether.service;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
     Optional<User> findByUserId(final String userId);
 }
