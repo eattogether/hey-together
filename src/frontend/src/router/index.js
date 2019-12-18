6a77adae67bef16cf3f3f43850334d6d76ba240f
CODEMCD <psbum1609@gmail.com>
2019-12-18 15:04:14 +0900

    feat: Login, ArticleForm 화면 라우터 추가

--- src/frontend/src/router/index.js
+++ src/frontend/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +11,22 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/404',
+    name: '404',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../components/404.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../components/Login.vue')
+  },
+  {
+    path: '/write',
+    name: 'write',
+    component: () => import('../components/ArticleForm.vue')
   }
 ]
 
