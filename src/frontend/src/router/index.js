89bf5990286a76ddac323aa8adcf167d850931ae
CODEMCD <psbum1609@gmail.com>
2019-12-18 17:43:14 +0900

    style: 주석 제거

--- src/frontend/src/router/index.js
+++ src/frontend/src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../components/Home.vue'
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
@@ -13,10 +13,7 @@ const routes = [
   {
     path: '/404',
     name: '404',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../components/404.vue')
+    component: () => import('../components/404.vue')
   },
   {
     path: '/login',
@@ -34,6 +31,6 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
 
 export default router
